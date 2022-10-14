8f0a884cf9f0c29ed7ec202ab780941f51138105
Mahmoud412 <mahmoudashour219@gmail.com>
2022-10-14 14:33:58 +0300

    init commit

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+
+import Icon from 'react-native-vector-icons/FontAwesome'
+import React from 'react';
+import {
+  SafeAreaView,
+  useColorScheme,
+} from 'react-native';
+
+
+
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  
+  return (
+    <SafeAreaView>
+      
+      
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
