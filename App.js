0950cf396dc6cd0847fcb42618632a1bc3b222f7
Mahmoud412 <mahmoudashour219@gmail.com>
2022-10-14 15:54:09 +0300

    added RanderPost

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
   useColorScheme,
 } from 'react-native';
 import HomeScreen from './src/screens/Home';
+import Post from './src/componets/Post';
 
 
 
@@ -25,7 +26,8 @@ const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
       
-      <HomeScreen/>
+    
+      <Post/>
     </SafeAreaView>
   );
 };
