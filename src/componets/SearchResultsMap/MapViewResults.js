33a1cd7a1b15c1659bbd581dcc4f3957bc44e93e
Mahmoud412 <mahmoudashour219@gmail.com>
2022-10-18 01:10:13 +0300

    styled FlatList

--- src/componets/SearchResultsMap/MapViewResults.js
+++ src/componets/SearchResultsMap/MapViewResults.js
@@ -1,12 +1,14 @@
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, useWindowDimensions} from 'react-native';
 import React, {useState} from 'react';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import styles from './styles';
 import places from '../../../assets/data/feed';
 import CustomMarker from './CustomMarker';
-import PostCardItem from '../Post/PostCardItem';
+import PostCardItem from '../PostCardItem';
 const MapViewResults = () => {
   const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+  const width = useWindowDimensions().width
+
   return (
     <View style={styles.container}>
       <MapView
@@ -27,8 +29,16 @@ const MapViewResults = () => {
           />
         ))}
       </MapView>
-      <View style={{position:'absolute' , bottom:10}}>
-        <PostCardItem post = {places[0]} />
+      <View style={{position: 'absolute', bottom: 10}}>
+        <FlatList
+          data={places}
+          renderItem={({item}) => <PostCardItem post={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={width-60}
+          snapToAlignment={'center'}
+          decelerationRate={'fast'}
+        />
       </View>
     </View>
   );
