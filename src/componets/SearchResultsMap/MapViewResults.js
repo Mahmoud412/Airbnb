6aaedda5a58b13262c0e835d71f3be16a4ff68a6
Mahmoud412 <mahmoudashour219@gmail.com>
2022-10-18 00:37:17 +0300

    Bug Fixed in CustomMarker

--- src/componets/SearchResultsMap/MapViewResults.js
+++ src/componets/SearchResultsMap/MapViewResults.js
@@ -1,24 +1,29 @@
 import {View, Text} from 'react-native';
-import React , {useState}from 'react';
+import React, {useState} from 'react';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import styles from './styles';
 import places from '../../../assets/data/feed';
 import CustomMarker from './CustomMarker';
 const MapViewResults = () => {
-    const [selectedPlaceId, setselectedPlaceId] = useState(null)
+  const [selectedPlaceId, setSelectedPlaceId] = useState(null);
   return (
     <View style={styles.container}>
       <MapView
         style={styles.mapContainer}
         provider={PROVIDER_GOOGLE}
         region={{
-            latitude: 28.3279822,
-            longitude: -16.5124847,
-            latitudeDelta: 0.8,
-            longitudeDelta: 0.8,
+          latitude: 28.3279822,
+          longitude: -16.5124847,
+          latitudeDelta: 0.8,
+          longitudeDelta: 0.8,
         }}>
-        {places.map(place=>(
-            <CustomMarker isSelected={place.id === selectedPlaceId} onPress={()=> selectedPlaceId(place.id)} coordinate={place.coordinate} price={place.newPrice}/>
+        {places.map(place => (
+          <CustomMarker
+            isSelected={place.id === selectedPlaceId}
+            onPress={() => setSelectedPlaceId(place.id)}
+            coordinate={place.coordinate}
+            price={place.newPrice}
+          />
         ))}
       </MapView>
     </View>
